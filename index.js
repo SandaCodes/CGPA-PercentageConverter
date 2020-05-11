6f4a1226c4525bbb61480c63ca00f97d54e13958
SandaCodes <uahmed977@gmail.com>
2020-05-11 10:22:33 -1100

    My first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+alert("Welcome");
+
+function convertToPercent(){
+
+  var x = document.querySelectorAll("input");
+
+  var y = document.querySelectorAll("input");
+
+    var yourGp = x[0].value;
+
+    var maxGp = y[1].value;
+
+    if (isNaN(yourGp) == false && isNaN(maxGp) == false) {
+
+      var yourCgpa = yourGp/maxGp;
+
+      var yourPercent = Math.floor(yourCgpa * 100);
+
+      alert("You have " + yourPercent + "%");
+    }
+    else {
+      alert("Input numbers only");
+    }
+}
